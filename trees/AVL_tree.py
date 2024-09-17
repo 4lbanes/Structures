6cbeedc17c926bbced706ed3278a57f069b8ed1d
albanez <arthurprogbr@gmail.com>
2024-09-17 12:26:45 -0300

    avl implemented

---
+++ trees/AVL_tree.py
@@ -0,0 +1,328 @@
+import tkinter as tk
+import time
+
+# Classe que representa um nó da árvore AVL
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.value = key
+        self.height = 1
+
+# Classe que representa a Árvore AVL
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def is_empty(self):
+        return self.root is None
+
+    def height(self, node):
+        if node is None:
+            return 0
+        return node.height
+
+    def update_height(self, node):
+        node.height = 1 + max(self.height(node.left), self.height(node.right))
+
+    def balance_factor(self, node):
+        if node is None:
+            return 0
+        return self.height(node.left) - self.height(node.right)
+
+    def right_rotation(self, y):
+        x = y.left
+        T2 = x.right
+        x.right = y
+        y.left = T2
+        self.update_height(y)
+        self.update_height(x)
+        return x
+
+    def left_rotation(self, x):
+        y = x.right
+        T2 = y.left
+        y.left = x
+        x.right = T2
+        self.update_height(x)
+        self.update_height(y)
+        return y
+
+    def balance(self, node):
+        bf = self.balance_factor(node)
+        if bf > 1:
+            if self.balance_factor(node.left) < 0:
+                node.left = self.left_rotation(node.left)
+            node = self.right_rotation(node)
+        elif bf < -1:
+            if self.balance_factor(node.right) > 0:
+                node.right = self.right_rotation(node.right)
+            node = self.left_rotation(node)
+        return node
+
+    def insert(self, key, gui=None):
+        if self.root is None:
+            self.root = Node(key)
+        else:
+            self.root = self._insert(self.root, key, gui)
+
+    def _insert(self, node, key, gui):
+        if node is None:
+            return Node(key)
+        if gui:
+            gui.highlight_node(node.value, "yellow")  # Destaca o nó atual
+            gui.update()
+            time.sleep(1)  # Pausa para o usuário ver a comparação
+
+            # Exibe comparação entre o valor atual e o valor inserido
+            if key < node.value:
+                gui.show_comparison(node.value, key, ">")
+                time.sleep(1)
+            else:
+                gui.show_comparison(node.value, key, "<")
+                time.sleep(1)
+
+        if key < node.value:
+            node.left = self._insert(node.left, key, gui)
+        else:
+            node.right = self._insert(node.right, key, gui)
+
+        self.update_height(node)
+        balanced_node = self.balance(node)
+
+        if gui and balanced_node != node:
+            gui.highlight_node(node.value, "red")  # Destaca o nó antes da rotação
+            gui.update()
+            time.sleep(1)  # Pausa para o usuário ver a rotação
+        
+        return balanced_node
+    
+    def inorder(self):
+        return self._inorder(self.root, [])
+
+    def _inorder(self, root, result):
+        if root:
+            self._inorder(root.left, result)
+            result.append(root.value)
+            self._inorder(root.right, result)
+        return result
+
+    def preorder(self):
+        return self._preorder(self.root, [])
+
+    def _preorder(self, root, result):
+        if root:
+            result.append(root.value)
+            self._preorder(root.left, result)
+            self._preorder(root.right, result)
+        return result
+
+    def postorder(self):
+        return self._postorder(self.root, [])
+
+    def _postorder(self, root, result):
+        if root:
+            self._postorder(root.left, result)
+            self._postorder(root.right, result)
+            result.append(root.value)
+        return result
+    
+    def delete(self, key, gui=None):
+        self.root = self._delete(self.root, key, gui)
+
+    def _delete(self, node, key, gui):
+        if node is None:
+            return node
+
+        if gui:
+            # Destaca o nó atual e exibe comparação
+            self._schedule_comparison(node, key, gui, time.sleep(0.5))
+            gui.highlight_node(node.value, "yellow")
+            gui.update()
+            time.sleep(1)
+
+        if key < node.value:
+            node.left = self._delete(node.left, key, gui)
+        elif key > node.value:
+            node.right = self._delete(node.right, key, gui)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+
+            temp = self.get_min_value_node(node.right)
+            node.value = temp.value
+            node.right = self._delete(node.right, temp.value, gui)
+
+        self.update_height(node)
+        balanced_node = self.balance(node)
+
+        if gui and balanced_node != node:
+            gui.highlight_node(node.value, "red")
+            gui.update()
+            time.sleep(1)
+
+        return balanced_node
+
+    def invert(self):
+        self._invert(self.root)
+
+    def _invert(self, root):
+        if root:
+            root.left, root.right = root.right, root.left
+            self._invert(root.left)
+            self._invert(root.right)
+
+# Classe que representa a Interface Gráfica
+class AVLGUI(tk.Tk):
+    def __init__(self, avl_tree):
+        super().__init__()
+        self.avl_tree = avl_tree
+        self.title("Visualização da Árvore AVL")
+
+        self.left_frame = tk.Frame(self)
+        self.left_frame.pack(side=tk.LEFT, fill=tk.Y, padx=10, pady=10)
+
+        self.canvas = tk.Canvas(self, width=800, height=600, bg="white")
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+
+        self.insert_label = tk.Label(self.left_frame, text="Inserir elemento:")
+        self.insert_label.pack()
+        self.insert_entry = tk.Entry(self.left_frame)
+        self.insert_entry.pack()
+        self.insert_button = tk.Button(self.left_frame, text="Inserir", command=self.insert_node)
+        self.insert_button.pack()
+
+        self.remove_label = tk.Label(self.left_frame, text="Remover elemento:")
+        self.remove_label.pack()
+        self.remove_entry = tk.Entry(self.left_frame)
+        self.remove_entry.pack()
+        self.remove_button = tk.Button(self.left_frame, text="Remover", command=self.remove_node)
+        self.remove_button.pack()
+        
+        self.invert_button = tk.Button(self.left_frame, text="Inverter Árvore", command=self.invert_tree)
+        self.invert_button.pack()
+
+        self.inorder_button = tk.Button(self.left_frame, text="In-order", command=self.show_inorder)
+        self.inorder_button.pack()
+
+        self.preorder_button = tk.Button(self.left_frame, text="Pre-order", command=self.show_preorder)
+        self.preorder_button.pack()
+
+        self.postorder_button = tk.Button(self.left_frame, text="Post-order", command=self.show_postorder)
+        self.postorder_button.pack()
+
+        self.output_text = tk.Text(self.left_frame, height=10, width=30)
+        self.output_text.pack()
+
+        self.node_radius = 20
+        self.horizontal_spacing = 50
+        self.vertical_spacing = 50
+        self.x_start = 400
+        self.y_start = 50
+        self.node_positions = {}
+        self.current_lines = []
+        self.draw_tree(self.avl_tree.root, self.x_start, self.y_start, 1)
+        self.canvas.bind("<Button-1>", self.on_click)
+
+    def draw_tree(self, node, x, y, level):
+        self.canvas.delete("all")
+        self.node_positions.clear()
+        self._draw_tree(node, x, y, level)
+
+    def _draw_tree(self, node, x, y, level):
+        if node:
+            self.node_positions[node.value] = (x, y)
+            self.canvas.create_oval(x - self.node_radius, y - self.node_radius,
+                                    x + self.node_radius, y + self.node_radius,
+                                    fill='white', outline='black', width=2)
+            self.canvas.create_text(x, y, text=str(node.value), font=("Arial", 12))
+
+            if node.left:
+                self.canvas.create_line(x, y + self.node_radius, x - self.horizontal_spacing / level,
+                                        y + self.vertical_spacing - self.node_radius)
+                self._draw_tree(node.left, x - self.horizontal_spacing / level, y + self.vertical_spacing, level + 1)
+
+            if node.right:
+                self.canvas.create_line(x, y + self.node_radius, x + self.horizontal_spacing / level,
+                                        y + self.vertical_spacing - self.node_radius)
+                self._draw_tree(node.right, x + self.horizontal_spacing / level, y + self.vertical_spacing, level + 1)
+
+    def on_click(self, event):
+        clicked_value = self.get_clicked_node(event.x, event.y)
+        if clicked_value:
+            print(f"Nó clicado: {clicked_value}")
+            self.avl_tree.delete(clicked_value)
+            self.draw_tree(self.avl_tree.root, self.x_start, self.y_start, 1)
+
+    def get_clicked_node(self, x, y):
+        for value, (vx, vy) in self.node_positions.items():
+            if (vx - self.node_radius) < x < (vx + self.node_radius) and (vy - self.node_radius) < y < (vy + self.node_radius):
+                return value
+        return None
+
+    def insert_node(self):
+        value = self.insert_entry.get()
+        if value.isdigit():
+            self.avl_tree.insert(int(value), gui=self)
+            self.draw_tree(self.avl_tree.root, self.x_start, self.y_start, 1)
+            self.insert_entry.delete(0, tk.END)
+            
+    def invert_tree(self):
+        self.avl_tree.invert()
+        self.draw_tree(self.avl_tree.root, self.x_start, self.y_start, 1)
+
+    def remove_node(self):
+        value = self.remove_entry.get()
+        if value.isdigit():
+            self.avl_tree.delete(int(value))
+            self.draw_tree(self.avl_tree.root, self.x_start, self.y_start, 1)
+            self.remove_entry.delete(0, tk.END)
+
+    def show_inorder(self):
+        self.show_traversal(self.avl_tree.inorder(), 'red')
+
+    def show_preorder(self):
+        self.show_traversal(self.avl_tree.preorder(), 'blue')
+
+    def show_postorder(self):
+        self.show_traversal(self.avl_tree.postorder(), 'green')
+
+    def show_traversal(self, traversal, color):
+        self.output_text.delete(1.0, tk.END)
+        for step in traversal:
+            self.output_text.insert(tk.END, f"{step}\n")
+        self.clear_previous_lines()  
+        self.animate_traversal(traversal, color)
+        
+    def clear_previous_lines(self):
+        for line in self.current_lines:
+            self.canvas.delete(line)
+        self.current_lines.clear()
+
+    def animate_traversal(self, traversal, color):
+        for i in range(len(traversal) - 1):
+            x1, y1 = self.node_positions[traversal[i]]
+            x2, y2 = self.node_positions[traversal[i + 1]]
+            line = self.canvas.create_line(x1, y1, x2, y2, fill=color, width=2, arrow=tk.LAST)
+            self.current_lines.append(line)
+
+    def highlight_node(self, value, color):
+        if value in self.node_positions:
+            x, y = self.node_positions[value]
+            self.canvas.create_oval(x - self.node_radius, y - self.node_radius,
+                                    x + self.node_radius, y + self.node_radius,
+                                    fill='white', outline=color, width=4)
+
+    def show_comparison(self, node_value, inserted_value, comparison):
+        self.output_text.delete(1.0, tk.END)
+        self.output_text.insert(tk.END, f"{node_value} {comparison} {inserted_value}\n")
+
+
+if __name__ == "__main__":
+    avl = AVLTree()
+    gui = AVLGUI(avl)
+    gui.mainloop()
+
+
